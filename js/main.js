430c8585bdd6742ec6297963f493cfb4688ddb11
NicholasConti <conti94@live.it>
2023-02-22 17:02:34 +0100

    milestone1-completed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 "use strict";
-//Variabil utente
+//Variabili utente
 const nomeUtente = document.getElementById('nome-utente').value;
 const kmUtente = Number(document.getElementById('km-utente').value);
-const etaUtente = (document.getElementById('eta-utente').value);
+let etaUtente;
 
-// Variabile prezzo
+// Variabili prezzo
 const prezzoBase = Number(0.21);
 let prezzoTot = kmUtente * prezzoBase;
 
@@ -14,11 +14,13 @@ const confermaUtente = document.getElementById('conferma');
 
 confermaUtente.addEventListener('click',
     function() {
-        if (etaUtente ===  'minorenne'){
+        etaUtente = (document.getElementById('eta-utente').value);
+        prezzoTot = kmUtente * prezzoBase;
+        if (etaUtente === 'minorenne'){
             prezzoTot = prezzoTot -(prezzoTot * 20 /100);
         } else if (etaUtente === 'over65'){
             prezzoTot = prezzoTot -(prezzoTot * 40 /100);
         }
-        document.getElementById("demo").innerHTML = prezzoTot;
+        document.getElementById("demo").innerHTML = prezzoTot.toFixed(2);
     }
 )
